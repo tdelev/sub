8b90d5066d0f86fd4c660ba71c15b035f747ac82
Tomche Delev <tomche.delev@sorsix.com>
2024-11-12 00:54:47 +0100

    ref/deref, struct, impl...

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,59 @@
 use core::fmt;
-use std::io::Write;
-use std::io::{self, BufRead};
+use std::borrow::Cow;
+use std::ffi::{OsStr, OsString};
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Write};
 use std::process;
 
-use clap::{crate_description, crate_name, crate_version, Arg, Command};
+use clap::Parser;
+use regex::RegexBuilder;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(help = "The search pattern that should be replaced")]
+    pattern: String,
+    #[arg(help = "The replacement string for the search pattern")]
+    replacement: String,
+    #[arg(help = "Input file(s) to perform the substitution on")]
+    files: Vec<OsString>,
+    #[arg(short, long, help = "Use case-insensitive search")]
+    ignore_case: bool,
+    #[arg(short = 'p', long, requires = "files", help = "Edit files in place")]
+    in_place: bool,
+    #[arg(short, long, help = "Only match the pattern on whole words")]
+    whole_word: bool,
+    #[arg(
+        short = 'm',
+        long = "match",
+        value_name = "pattern",
+        help = "Only substitute on lines that match the pattern"
+    )]
+    line_match: Option<String>,
+}
+
+struct Sub<'a> {
+    pattern: &'a str,
+    replacement: &'a str,
+    in_place: bool,
+    whole_word: bool,
+    ignore_case: bool,
+    match_pattern: Option<&'a str>,
+    inputs: Vec<Input<'a>>,
+}
 
 #[derive(Debug, Clone)]
 enum SubError {
     FailedToWrite,
     InvalidUTF8,
+    RegexError,
+    FileNotFoundError(OsString),
+}
+
+#[derive(Debug, Clone)]
+enum Input<'a> {
+    StdIn,
+    File(&'a OsStr),
 }
 
 impl fmt::Display for SubError {
@@ -18,43 +63,110 @@ impl fmt::Display for SubError {
         match self {
             FailedToWrite => write!(f, "Output stream has been closed"),
             InvalidUTF8 => write!(f, "Input contains invalid UTF-8"),
+            RegexError => write!(f, "Regex error"),
+            FileNotFoundError(path) => write!(f, "Can not open file '{}'", path.to_string_lossy()),
         }
     }
 }
 
 type Result<T> = std::result::Result<T, SubError>;
 
-fn run(pattern: &str, replacement: &str) -> Result<()> {
-    let stdin = io::stdin();
-    let stdout = io::stdout();
-    let input = stdin.lock();
-    let mut output = stdout.lock();
+impl<'a> Sub<'a> {
+    fn run(self, is_tty: bool) -> Result<()> {
+        let pattern = if self.whole_word {
+            format!(r"\b{}\b", self.pattern)
+        } else {
+            self.pattern.to_string()
+        };
+        let stdin = io::stdin();
+        let stdout = io::stdout();
+        let re = RegexBuilder::new(&pattern)
+            .case_insensitive(self.ignore_case)
+            .build()
+            .map_err(|_| SubError::RegexError)?;
 
-    for line in input.lines() {
-        let line = line.map_err(|_| SubError::InvalidUTF8)?;
-        let new_line = line.replace(pattern, replacement);
-        writeln!(output, "{}", new_line).map_err(|_| SubError::FailedToWrite)?;
+        let line_match_pattern = self
+            .match_pattern
+            .map(|p| {
+                RegexBuilder::new(&p)
+                    .case_insensitive(self.ignore_case)
+                    .build()
+                    .map_err(|_| SubError::RegexError)
+            })
+            .transpose()?;
+        for input in self.inputs.iter() {
+            if is_tty {
+                let mut reader = create_reader(input, &stdin)?;
+                let mut output = stdout.lock();
+                self.replace(&re, &line_match_pattern, &mut reader, &mut output)?;
+            } else {
+                let mut reader = create_reader(input, &stdin)?;
+                let mut writer = io::BufWriter::new(stdout.lock());
+                self.replace(&re, &line_match_pattern, &mut reader, &mut writer)?;
+            }
+        }
+
+        Ok(())
     }
 
-    Ok(())
+    fn replace(
+        &self,
+        re: &regex::Regex,
+        line_match_pattern: &Option<regex::Regex>,
+        reader: &mut dyn BufRead,
+        writer: &mut dyn Write,
+    ) -> Result<()> {
+        let mut line_buffer = String::new();
+        loop {
+            line_buffer.clear();
+            let num_bytes = reader
+                .read_line(&mut line_buffer)
+                .map_err(|_| SubError::InvalidUTF8)?;
+            if num_bytes == 0 {
+                break;
+            }
+            let new_line = if line_match_pattern
+                .as_ref()
+                .map_or(true, |m| m.is_match(&line_buffer))
+            {
+                re.replace_all(&line_buffer, self.replacement)
+            } else {
+                Cow::from(&line_buffer)
+            };
+            write!(writer, "{}", new_line).map_err(|_| SubError::FailedToWrite)?;
+        }
+        Ok(())
+    }
+}
+
+fn create_reader(input: &Input<'_>, stdin: &io::Stdin) -> Result<Box<dyn BufRead>> {
+    let reader: Box<dyn BufRead> = match input {
+        Input::StdIn => Box::new(stdin.lock()),
+        Input::File(path) => {
+            let f = File::open(path).map_err(|_| SubError::FileNotFoundError(path.into()))?;
+            Box::new(BufReader::new(f))
+        }
+    };
+    Ok(reader)
 }
 
 fn main() {
-    let app = Command::new(crate_name!())
-        .version(crate_version!())
-        .about(crate_description!())
-        .args([
-            Arg::new("pattern")
-                .required(true)
-                .help("The search pattern that should be replaced"),
-            Arg::new("replacement")
-                .required(true)
-                .help("The string that should be substituted in"),
-        ]);
-    let matches = app.get_matches();
-    let pattern = matches.get_one::<String>("pattern").unwrap();
-    let replacement = matches.get_one::<String>("replacement").unwrap();
-    let result = run(pattern, replacement);
+    let cli = Cli::parse();
+    let inputs = if cli.files.is_empty() {
+        vec![Input::StdIn]
+    } else {
+        cli.files.iter().map(|f| Input::File(f)).collect()
+    };
+    let sub = Sub {
+        pattern: &cli.pattern,
+        replacement: &cli.replacement,
+        in_place: cli.in_place,
+        whole_word: cli.whole_word,
+        ignore_case: cli.ignore_case,
+        match_pattern: cli.line_match.as_deref(),
+        inputs,
+    };
+    let result = sub.run(atty::is(atty::Stream::Stdout));
     match result {
         Ok(_) => {}
         Err(e) => {
